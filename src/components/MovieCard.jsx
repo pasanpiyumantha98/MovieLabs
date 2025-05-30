0146a0032f6cf02ec031f337e0a0a044dc5488f1
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-05-30 12:10:30 +0530

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,14 +2,22 @@ export default MovieCard
 
 function MovieCard ({movie}) {
 
+function FavouriteButton () {
+    alert("Added to the favourits!")
+}
+
 return(
 <div className="movie-card">
 <div className="movie-paster">
     
-    <img src={movie.img}/>
+    <img src={movie.url}/>
     <div className="movie-overlay">
         <button className="favourite-btn" onClick={FavouriteButton}>FF</button>
     </div>
+</div>
+<div className="movie-info">
+    <h3>{movie.title}</h3>
+    <p>{movie.release_date}</p>
 </div>
 </div>
 )
