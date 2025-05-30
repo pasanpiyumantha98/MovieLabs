31e8939cf7699cecb3342a126d69b9e06d29df81
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-05-30 12:27:42 +0530

    moviee

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+export default Home
+
+function Home () {
+
+movies = [{id:1, title:"Harry Potter", release_date:"2025-05-30"},
+         { id:2, title:"Harry Potter", release_date:"2025-05-30"},
+            {id:3, title:"Harry Potter", release_date:"2025-05-30"}]
+
+
+return(
+
+
+
+
+
+)
+}
