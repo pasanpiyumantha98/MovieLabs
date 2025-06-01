ae0174aa8d43507476cdd35d7c9616f23e57e982
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-01 20:13:34 +0530

    ui 3

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -6,9 +6,9 @@ function Help()
 return(
 <div class="container">
   <div class="row">
-    <div class="col-sm-3 col-md-3">....</div>
+    <div class="col-sm-3 col-md-4">....</div>
     <div class="col-sm-3 col-md-6">....</div>
-    <div class="col-sm-3 col-md-3">....</div>
+    <div class="col-sm-3 col-md-2">....</div>
   </div>
 </div>  
 )
