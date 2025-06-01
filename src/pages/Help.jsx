49426fbfb5223138a8d6e6106b19464351841427
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-01 20:14:07 +0530

    Update Help.jsx

--- src/pages/Help.jsx
+++ src/pages/Help.jsx
@@ -7,8 +7,7 @@ return(
 <div class="container">
   <div class="row">
     <div class="col-sm-3 col-md-4">....</div>
-    <div class="col-sm-3 col-md-6">....</div>
-    <div class="col-sm-3 col-md-2">....</div>
+    <div class="col-sm-3 col-md-6">....</div> <div class="col-sm-3 col-md-2">....</div>
   </div>
 </div>  
 )
