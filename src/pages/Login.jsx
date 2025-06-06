66b07df71ad2005c21318c3f76dd897d954c69ad
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-06 10:20:05 +0530

    back 1

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,28 @@
 import '../css/Login.css'
 import logo from '../assets/logo.png'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 function Login () {
+
+  const [uname,setUname] = useState("")
+  const [pass,setPass] = useState("")
+
+
+  async function submitLogin(e)
+  {
+
+     e.preventDefault();
+
+   const person = await axios.post('http://localhost:9000/user/login', {Username:uname,Password:pass})
+
+   if(person)
+    alert("success")
+   else
+   alert("NotOK")
+
+  }
 
 
 
@@ -15,13 +35,13 @@ return(
     <br></br>
     <center><h3>Your personal movie manager</h3></center>
     <br></br>
-      <form action="https://example.com/support" method="POST" autocomplete="on" novalidate>
+      <form autocomplete="on" novalidate>
     <label for="name" class="required">Username</label>
-    <input id="username" name="name" type="text"  autocomplete="name" />
+    <input id="username" name="name" type="text"  autocomplete="name" value={uname} onChange={(e) => setUname(e.target.value)}/>
     <label for="email" class="required">Password</label>
-    <input id="pass" name="password" type="password" required placeholder="" />
+    <input id="pass" name="password" type="password" required value={pass} onChange={(p) => setPass(p.target.value)} />
     
-   <center> <button type="submit">login</button> <Link to="/signup"> <button >Not Registered?</button></Link></center>
+   <center> <button onClick={submitLogin}>login</button> <Link to="/signup"> <button >Not Registered?</button></Link></center>
   </form>
     </div>
   </div>
