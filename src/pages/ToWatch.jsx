39dbf8d2b77b1178b3a4d99a24e27551067f0234
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-07 08:46:59 +0530

    Update ToWatch.jsx

--- src/pages/ToWatch.jsx
+++ src/pages/ToWatch.jsx
@@ -1,14 +1,13 @@
 import '../css/Favorites.css'
-import { useMovieContext } from '../contexts/moviecontext'
 import MovieCard from '../components/MovieCard'
 import { useAuth } from '../contexts/authcontext'
 import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import NavBar from "../components/NavBar"
 import axios from 'axios'
-import { useAuth } from '../contexts/authcontext'
 
-function Watched () 
+
+function ToWatch () 
 {
 
 const {user} = useAuth()
@@ -21,23 +20,24 @@ useEffect(() =>{
 if(!user){
   navigate("/")
 }
-
-
-
-
 },[user,navigate])
 
+useEffect(()=>{
+
 const watched = axios.get('/movies/watchedlist', {Username:user.Username})
+
+},[])
+
 
     
 return(
    <div>
    <NavBar/>
  <div className="movies-grid">
-            {watched.map((movie) => (<MovieCard  movie={movie} key={movie.id}/>))}
+      
   </div>
   </div>
 )
 }
 
-export default Watched
+export default ToWatch
