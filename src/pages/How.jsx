5cbe58519dae8357aa579fceca9ca38738a3dfea
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-06-08 18:24:32 +0530

    Update How.jsx

--- src/pages/How.jsx
+++ src/pages/How.jsx
@@ -23,23 +23,9 @@ return(
   <NavBar/>
   <div class="row">
     <div class="helpcc">
-    <h2>Need support ?</h2>
+    <h2>How Movie Labs Work?</h2>
     <br></br>
-      <form >
-    <label for="name" class="required">Full Name</label>
-    <input id="name" name="name" type="text" required placeholder="Jane Smith" />
-    <label for="email" class="required">Email Address</label>
-    <input id="email" name="email" type="email" required placeholder="jane@example.com" />
-    <label for="phone">Phone (optional)</label>
-    <input id="phone" name="phone" type="tel" placeholder="+94 77 123 4567" autocomplete="tel" />
-    <label for="subject" class="required">Subject</label>
-    <input id="subject" name="subject" type="text" maxlength="120" required placeholder="Billing question, technical issue, feedback…" />
-    
-    <label for="message" class="required">Message</label>
-    <textarea id="message" name="message" rows="6" required placeholder="Tell us what’s happening…"></textarea>
-   
-    <button type="submit">Submit</button>
-  </form>
+     
     </div>
   </div>
 </div>  
